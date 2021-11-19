080ef48f88a76765652e5583b292be7c8fe90cc7
Vanessa Rodriguez <vane.vrb@gmail.com>
2021-11-19 07:20:55 -0500

    Update routes.js

--- ej_mysql_conn/routes.js
+++ ej_mysql_conn/routes.js
@@ -12,4 +12,39 @@ routes.get("/",(req,res)=>{
     })
 })
 
+routes.post("/", (req,res)=>{
+    req.getConnection((err,conn) =>{
+        if (err) return res.send(err)
+        console.log([req.body])
+        conn.query("INSERT INTO numeros set ?",[req.body],(err,rows)=>{
+            if(err) return res.send(err)
+            res.json(rows)
+        })
+    })
+}
+)
+routes.put("/:id",(req,res)=>{
+    req.getConnection((err,conn) =>{
+        if (err) return res.send(err)
+        console.log("valor editado:")
+        console.log([req.params.id])
+        console.log([req.body])
+        conn.query("UPDATE numeros set ? where ID = ?", [req.body,req.params.id],(err,rows)=>{
+            if(err) return res.send(err)
+            res.json(rows)
+        })
+    })
+})
+routes.delete("/:id",(req,res)=>{
+    req.getConnection((err,conn) =>{
+        if (err) return res.send(err)
+        console.log("valor borrado:")
+        console.log([req.params.id])
+        // console.log([req.body])
+        conn.query("DELETE FROM numeros where ID = ?",[req.params.id],(err,rows)=>{
+            if(err) return res.send(err)
+            res.json(rows)
+        })
+    })
+})
 module.exports = routes;
